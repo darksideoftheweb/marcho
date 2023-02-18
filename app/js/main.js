098a49f7a6988852d28fd4b576e24d2c0b81a5f3
Anton <toxa910212@yandex.ua>
2023-02-18 13:22:13 +0200

    addet shop-page

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,34 @@
 console.log('hi')
 $(function () {
+
+  $('.shop-content__filter-btn').on('click', function () {
+    $('.shop-content__filter-btn').removeClass('shop-content__filter-btn--active')
+    $(this).addClass('shop-content__filter-btn--active')
+  })
+
+  $('.button-list').on('click', function () {
+    $('.product-item').addClass('product-item--list')
+  })
+
+  $('.button-grid').on('click', function () {
+    $('.product-item').removeClass('product-item--list')
+  })
+
+  $('.filter-price__input').ionRangeSlider({
+    type: "double",
+    prefix: '$',
+    onChange: function (data) {
+      $('.filter-price__from').text(data.from);
+      $('.filter-price__to').text(data.to);
+    },
+    onStart: function (data) {
+      $('.filter-price__from').text(data.from);
+      $('.filter-price__to').text(data.to);
+    }
+  })
+
+
+
   $('.top-slider__inner').slick({
     dots: true,
     arrows: false,
@@ -14,6 +43,8 @@ $(function () {
     readOnly: true
   });
 
+
+  $('.select-style').styler();
 
 
   const countDownClock = (number = 100, format = 'seconds') => {
